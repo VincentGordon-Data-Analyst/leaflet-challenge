ddab8c726407783da0cc577fa66e376466a6f486
vincentgordon97 <66184004+vincentgordon97@users.noreply.github.com>
2023-02-15 19:00:18 -0500

    Fixed legend

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -3,8 +3,8 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Creating the map object
 var myMap = L.map("map", {
-  center: [37.96044, -112.30695],
-  zoom: 5
+  center: [37.96044, -122.30695],
+  zoom: 6
 });
 
 // Adding the tile layer
@@ -12,18 +12,20 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-function markerSize(magnitude) {
-  return magnitude * 10000;
+// Define the size of each magnitude
+function markerSize(mag) {
+  return mag * 10000;
 }
 
+// Define the colors for the detph of each magnitude
 function colorPicker(d) {
-  return d > 100 ? "#d73027":
-  d > 50 ? "#fc8d59":
-  d > 25 ? "#fee08b":
-  d > 12 ? "#ffffbf":
-  d > 5 ? "#d9ef8b":
-  d > 0 ? "#91cf60":
-          "#1a9850";
+  return d > 1000 ? "#d73027":
+         d > 500 ? "#fc8d59":
+         d > 300 ? "#fee08b":
+         d > 120 ? "#ffffbf":
+         d > 60 ? "#d9ef8b":
+         d > 30 ? "#91cf60":
+                  "#66bd63";
 }
 
 // Perform a GET request to the query URL
@@ -38,29 +40,31 @@ d3.json(queryUrl).then(function (data) {
     var properties = feature.properties;
     var depth = feature.geometry.coordinates[2];  
 
+    // Plot a circle for each earthquake
     L.circle([location[1], location[0]], {
-      weight: 2,
-      fillOpacity: 0.7,
-      color: colorPicker(depth),
+      weight: 0.55,
+      fillOpacity: 0.75,
+      color: 'black',
       fillColor: colorPicker(depth),
       radius: markerSize(properties.mag)
     }).bindPopup(`<h3>${properties.title}</h3><hr><h4>${properties.place}</h4>`).addTo(myMap);  
   }
 
+  // Add a legend
   var legend = L.control({position: "bottomright"});
-
   legend.onAdd = function() {
     var div = L.DomUtil.create('div', 'info legend'),
-    severity = [0, 5, 12, 25, 50, 100];
+    severity = [0, 30, 60, 120, 300, 500, 1000];
     labels = [];
 
+    // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < severity.length; i++) {
       div.innerHTML +=
           '<i style="background:' + colorPicker(severity[i] + 1) + '"></i> ' +
           severity[i] + (severity[i + 1] ? '&ndash;' + severity[i + 1] + '<br>' : '+');
-  }
+    }
     return div;
-  }
+  };
   legend.addTo(myMap);
 
 });
